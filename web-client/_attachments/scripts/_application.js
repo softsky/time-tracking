de9235a542e34dc9cf816d50ec84b4fe2c12d88c
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-04-18 20:48:01 +0300

    provider added, simple rows output added

--- web-client/_attachments/scripts/_application.js
+++ web-client/_attachments/scripts/_application.js
@@ -13,52 +13,63 @@ angular.module('webClientApp', [
     Date.prototype.getWeek = function() {
         var onejan = new Date(this.getFullYear(), 0, 1);
         return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
-    }
-}).config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-        })
-        .when('/login', {
-            templateUrl: 'views/login.html',
-            controller: 'LoginCtrl'
-        })
-        .when('/calendar', {
-            templateUrl: 'views/calendar.html',
-            controller: 'CalendarCtrl'
-        })
-        .when('/day/:year/:month/:day', {
-            templateUrl: 'views/day.html',
-            controller: 'DayCtrl'
-        })
-        .when('/day/', {
-            redirectTo: function(){
-                var date = new Date()
-                return ['/day', date.getFullYear(), (date.getMonth() + 1), date.getDate()].join('/');
-            }
-        })
-        .when('/week/:year/:week', {
-            templateUrl: 'views/week.html',
-            controller: 'WeekCtrl'
-        })
-        .when('/week', {
-            redirectTo: function(){
-                var date = new Date()
-                return ['/week', date.getFullYear(), date.getWeek()].join('/');
-            }
-        })
-        .when('/month/:year/:month', {
-            templateUrl: 'views/month.html',
-            controller: 'MonthCtrl'
-        })
-        .when('/month', {
-            redirectTo: function(){
-                var date = new Date()
-                return ['/month', date.getFullYear(), (date.getMonth() + 1)].join('/');
-            }
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-})
+    }}
+
+)
+    .provider('$data', function (){
+	function Provider(cornercouch){
+	    this.db = cornercouch('http://vm81.softsky.com.ua:5984').getDB('softsky_timetracking');
+	    this.db.method = "GET";
+	}
+	this.$get = function(cornercouch){
+	    return new Provider(cornercouch)
+	}
+    })
+    .config(function ($routeProvider) {
+	$routeProvider
+            .when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl'
+            })
+            .when('/login', {
+		templateUrl: 'views/login.html',
+		controller: 'LoginCtrl'
+            })
+            .when('/calendar', {
+		templateUrl: 'views/calendar.html',
+		controller: 'CalendarCtrl'
+            })
+            .when('/day/:year/:month/:day', {
+		templateUrl: 'views/day.html',
+		controller: 'DayCtrl'
+            })
+            .when('/day/', {
+		redirectTo: function(){
+                    var date = new Date()
+                    return ['/day', date.getFullYear(), (date.getMonth() + 1), date.getDate()].join('/');
+		}
+            })
+            .when('/week/:year/:week', {
+		templateUrl: 'views/week.html',
+		controller: 'WeekCtrl'
+            })
+            .when('/week', {
+		redirectTo: function(){
+                    var date = new Date()
+                    return ['/week', date.getFullYear(), date.getWeek()].join('/');
+		}
+            })
+            .when('/month/:year/:month', {
+		templateUrl: 'views/month.html',
+		controller: 'MonthCtrl'
+            })
+            .when('/month', {
+		redirectTo: function(){
+                    var date = new Date()
+                    return ['/month', date.getFullYear(), (date.getMonth() + 1)].join('/');
+		}
+            })
+            .otherwise({
+		redirectTo: '/'
+            });
+    })
--- web-client/_attachments/scripts/controllers/day.js
+++ web-client/_attachments/scripts/controllers/day.js
@@ -1,9 +1,37 @@
 'use strict';
 
+function this_month_start(){
+    var now = new Date()
+    return new Date(now.getYear(), now.getMonth(), 1).getTime();
+}
+
+function this_month_end(){
+    var now = new Date()
+    return new Date(now.getYear(), now.getMonth(), 31).getTime();
+}
+
 angular.module('webClientApp')
-  .controller('DayCtrl', function ($scope, $location, $routeParams, $log) {
-      $log.info($routeParams);
-      $scope.year = $routeParams.year;
-      $scope.month = $routeParams.month;
-      $scope.day = $routeParams.day;
-  });
+    .config(function($dataProvider){
+	// doing some config on $dataProvider
+    })
+    .controller('DayCtrl',function ($scope, $location, $routeParams, $log, $data) {
+	$log.info($routeParams);
+	$scope.year = $routeParams.year;
+	$scope.month = $routeParams.month;
+	$scope.day = $routeParams.day;
+
+	$scope.db = $data.db;
+	$scope.$watch("db.rows", function(newVal){
+	    $log.info(newVal);
+	}, true)
+	
+	var thisDate = new Date($scope.year, parseInt($scope.month) - 1, $scope.day).getTime()
+	$scope.db.query("web-client", "byDateAndUsername", {
+            include_docs: true, 
+            descending: false, 
+            limit: 20, 
+            start_key: "[\""  + thisDate + "\", \"skekes\"]", 
+            start_end: "[\""  + thisDate + (24 * 3600 * 1000) + "\", \"skekes\"]",  // adding 24 hours day
+	})
+  })
+
