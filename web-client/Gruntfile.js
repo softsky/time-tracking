8e60cdca7626e3c885629251e8ac3e348286cd8d
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-05-06 07:45:45 +0300

    files now lies in app, dist is built to _attachments

--- web-client/Gruntfile.js
+++ web-client/Gruntfile.js
@@ -21,8 +21,8 @@ module.exports = function (grunt) {
     // Project settings
     yeoman: {
       // configurable paths
-      app: require('./bower.json').appPath || '_attachments',
-      dist: 'dist'
+      app: require('./bower.json').appPath || 'app',
+      dist: '_attachments'
     },
 
     // Watches files for changes and runs tasks based on the changed files
