2a29887efe9b15c7a50db9f9146686df39fa8a22
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-05-06 07:52:17 +0300

    initial test creation

--- web-client/test/spec/controllers/calendar.js
+++ web-client/test/spec/controllers/calendar.js
@@ -16,7 +16,7 @@ describe('Controller: CalendarCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should use uiConfig', function () {
+    expect(scope.uiConfig).not.toBe(undefined);
   });
 });
