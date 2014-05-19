8c40b1b379c87df93630b2a2c22bc62c51c7c241
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-05-19 12:25:21 +0300

    grunt-connect-proxy added. proxy configured

--- web-client/Gruntfile.js
+++ web-client/Gruntfile.js
@@ -65,13 +65,43 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
+        proxies: [
+            {
+                context: ['/_session', '/_utils', '/_users', '/softsky_timetracking'],
+                host: 'vm81.softsky.com.ua',
+                port: 5984,
+                https: false,
+                changeOrigin: false,
+                xforward: false
+            }
+        ],
+
       livereload: {
         options: {
           open: true,
           base: [
             '.tmp',
             '<%= yeoman.app %>'
-          ]
+          ],
+            middleware: function (connect, options) {
+                if (!Array.isArray(options.base)) {
+                    options.base = [options.base];
+                }
+
+                // Setup the proxy
+                var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+                // Serve static files.
+                options.base.forEach(function(base) {
+                    middlewares.push(connect.static(base));
+                });
+
+                // Make directory browse-able.
+                var directory = options.directory || options.base[options.base.length - 1];
+                middlewares.push(connect.directory(directory));
+
+                return middlewares;
+            }
         }
       },
       test: {
@@ -330,9 +360,12 @@ module.exports = function (grunt) {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
 
+      grunt.loadNpmTasks('grunt-connect-proxy');
+
     grunt.task.run([
       'clean:server',
       'bower-install',
+      'configureProxies:server',
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
